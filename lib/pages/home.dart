f4a50000e7b6fee15375dcca2d81afcc04901941
jomarindt <jomarinterremalakas@gmail.com>
2024-02-02 13:05:24 +0800

    added some files

---
+++ lib/pages/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageTitle(),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "GoMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage("assets/images/astro.png"),
+        ),
+      ),
+    );
+  }
+}
